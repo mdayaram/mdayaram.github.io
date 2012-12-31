63d2264badb3548425329a023b3a8c5df3c2735a
Manoj Dayaram <noj@moovweb.com>
2012-12-31 02:47:53 -0800

    Added recaptcha for email, and updated index page

--- helpers/images.rb
+++ helpers/images.rb
@@ -1,15 +1,25 @@
 module Images
   @@counts = {
     :profile => 5,
-    :blog => 5
+    :project => 5,
+    :blog => 5,
+    :random => 5
   }
 
   def get_profile_imgs
     get_imgs(:profile)
   end
 
+  def get_project_imgs
+    get_imgs(:project)
+  end
+
   def get_blog_imgs
     get_imgs(:blog)
+  end
+
+  def get_random_imgs
+    get_imgs(:random)
   end
 
   private
