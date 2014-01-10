fe3cab3a0a9f3f50464fae5bd192031d6823a59f
Manoj Dayaram <noj@moovweb.com>
2014-01-09 20:57:39 -0800

    same goes for sleepies

--- helpers/currently.rb
+++ helpers/currently.rb
@@ -10,7 +10,7 @@ module Currently
 
   def currently_doing?
     time = Time.now.utc + Time.zone_offset("PST")
-    if 3 <= time.hour and time.hour <= 8
+    if 3 <= time.hour and time.hour < 8
       return @@sleeping
     elsif 1 <= time.wday and time.wday <= 5 and 10 <= time.hour and time.hour < 20
       return @@working
