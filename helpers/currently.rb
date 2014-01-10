df896b44f62b7460b66f8e42ee3273267ab12624
Manoj Dayaram <noj@moovweb.com>
2014-01-09 20:56:13 -0800

    <= 20 means I work until nine, not what I meant, though that could happen...

--- helpers/currently.rb
+++ helpers/currently.rb
@@ -12,7 +12,7 @@ module Currently
     time = Time.now.utc + Time.zone_offset("PST")
     if 3 <= time.hour and time.hour <= 8
       return @@sleeping
-    elsif 1 <= time.wday and time.wday <= 5 and 10 <= time.hour and time.hour <= 20
+    elsif 1 <= time.wday and time.wday <= 5 and 10 <= time.hour and time.hour < 20
       return @@working
     end
     return @@random.sample
