5db633653fe2ee6358a9b8b5b011d887b21c9fb6
Manoj Dayaram <noj@moovweb.com>
2014-08-30 19:30:02 -0700

    updated the 'currently doing' section

--- helpers/currently.rb
+++ helpers/currently.rb
@@ -3,17 +3,13 @@ require 'yaml'
 module Currently
   @@random = YAML.load_file("#{settings.root}/private/currently.yml")
   @@sleeping = "sleeping, or at least trying to"
-  @@working = "writing software for <a href='http://www.youtube.com/user/moovweb' target='_blank'>moovweb</a>"
 
   @@random << @@sleeping
-  @@random << @@working
 
   def currently_doing?
     time = Time.now.utc + Time.zone_offset("PST")
     if 3 <= time.hour and time.hour < 8
       return @@sleeping
-    elsif 1 <= time.wday and time.wday <= 5 and 10 <= time.hour and time.hour < 20
-      return @@working
     end
     return @@random.sample
   end
